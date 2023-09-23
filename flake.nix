223be80bf4f0e67db1728a66b8e7f8f9a8985fdc
Daniel Emery <danielremery@gmail.com>
2023-09-23 09:27:28 +0200

    Add nix development flake
    Helps users using nix get up and running FAST on the project

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "blogindex.xyz development environment";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = f: genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [ pkgs.bashInteractive ];
+            buildInputs = with pkgs; [
+              mariadb-connector-c
+              postgresql
+              python3
+            ];
+            shellHook = with pkgs; ''
+              mkdir -p logs
+              python -m venv .venv
+              source .venv/bin/activate
+              pip install -r requirements.txt
+            '';
+          };
+        });
+    };
+}
+
