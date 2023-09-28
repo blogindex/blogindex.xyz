3a30f0e0ae72e8d2e9d5465ff148521e36751729
BeardedTek <kenny@beardedtek.com>
2023-09-28 12:32:10 -0800

    asdfasdfdasfasdf

--- dev/drone/test.sh
+++ dev/drone/test.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update && apt-get -y install postgresql-client
-loops=0
-export PG_PASS=${DATABASE_DB_PASS}
-DB_WAIT=99
-until [ "DB_UP" = "0" ]; do
-    DB_WAIT=$(pg_isready -d ${DATABASE_DB} -h ${DATABASE_DB_HOST} -U ${DATABASE_DB_USER})
-    echo "Waiting for database to come up. ~(${loops}/30 seconds) pg_isready exit_code=${DB_UP}"
-    sleep 1
-    ((loops++))
-    if [ loops -gt 29 ]; then
-        DB_WAIT=0
-    fi
-done
 
 cd /drone/src
 python -m venv .test
