13f3fcfd252cb23c3b62d98699e6310e153d727d
BeardedTek <kenny@beardedtek.com>
2023-09-28 12:25:21 -0800

    dfgdfgdfg

--- dev/drone/test.sh
+++ dev/drone/test.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get -y install postgresql-client
 loops=0
 export PG_PASS=${DATABASE_DB_PASS}
-until [ "$(pg_isready -h ${DATABASE_DB_HOST} -u ${DATABASE_DB_USER} -d ${DATABASE_DB})" = "0" ] || [ loops = "120" ]; do
+until [ "$(pg_isready -h ${DATABASE_DB_HOST} -U ${DATABASE_DB_USER} -d ${DATABASE_DB})" = "0" ] || [ loops = "120" ]; do
 
     echo "Waiting for database to come up. ~(${loops}/120 seconds)"
     sleep 1
