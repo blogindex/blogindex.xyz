2f560349140540435aa40fe0c2100e44477187c0
BeardedTek <kenny@beardedtek.com>
2023-09-28 13:20:13 -0800

    sdfgsdfgsdfgsdg

--- dev/drone/test.sh
+++ dev/drone/test.sh
@@ -18,7 +18,7 @@ cd /drone/src
 python -m venv .test
 source .test/bin/activate
 pip install -r requirements.txt
-PYTHONPATH=. pytest --exitfirst --verbose --failed-first --cov . --cov-report html
+PYTHONPATH=. pytest --exitfirst -vv --cov . --cov-report html
 
 if [ -d "/drone/src/htmlcov" ]; then
     echo "Copying htmlcov to ${RESULT_LOCATION}"
