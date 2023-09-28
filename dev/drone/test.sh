2b469149ead02c36ef1e85ac843a160c00205d1d
BeardedTek <kenny@beardedtek.com>
2023-09-28 13:03:19 -0800

    hopefully...

--- dev/drone/test.sh
+++ dev/drone/test.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 apt-get update && apt-get -y install postgresql-client
 export PG_PASS=${DATABASE_DB_PASS}
 WAITLOOP=99
@@ -14,7 +13,7 @@ cd /drone/src
 python -m venv .test
 source .test/bin/activate
 pip install -r requirements.txt
-pytest --exitfirst --verbose --failed-first --cov . --cov-report html
+PYTHONPATH=. pytest --exitfirst --verbose --failed-first --cov . --cov-report html
 
 if [ -d "/drone/src/htmlcov" ]; then
     echo "Copying htmlcov to ${RESULT_LOCATION}"
