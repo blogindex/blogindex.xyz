db53c234e36ce4aeaf3ab9e0e09e17dc20285443
BeardedTek <kenny@beardedtek.com>
2023-10-05 10:01:57 -0800

    Code Tidying Remove /docker/blogindex/example.env Add Metadata to API More clear logging in docker entrypoint script

--- api/main.py
+++ api/main.py
@@ -3,7 +3,27 @@ from starlette.middleware.sessions import SessionMiddleware
 
 from .routers import authors, sites, auth
 
+description= """
+API to read, add, edit, and delete blog sites, posts, and authors.
+
+See [Our GitHub Repository](https://github.com/blogindex/blogindex.xyz) for more information.
+"""  # noqa: E501
+
 app = FastAPI(
+    title="The Blog Index",
+    description=description,
+    summary="The Blog Index API - Find, Add, Update Blog Listsings",
+    version="0.0.1-pre-alpha",
+    contact={
+        "name": "BeardedTek (William Kenny)",
+        "url": "https://theblogindex.org",
+        "email": "api@theblogindex.org"
+    },
+    license_info={
+        "name": "GNU Affero General Public License",
+        "url": "https://www.gnu.org/licenses/agpl-3.0.en.html"
+    },
+
 )
 root_path = app.root_path
 
